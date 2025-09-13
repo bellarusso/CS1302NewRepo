d901970320e428c42468ac22ff3068a774a77ec5
bellarusso <Irusso1@my.westga.edu>
2025-09-12 21:09:54 -0400

    Specs for BillCalculator class

--- Lab4/src/edu/westga/cs1302/bill/model/BillCalculator.java
+++ Lab4/src/edu/westga/cs1302/bill/model/BillCalculator.java
@@ -1,5 +1,10 @@
 package edu.westga.cs1302.bill.model;
 
 public class BillCalculator {
-
+/** 
+ * gives static method to calculate bill total
+ * 
+ * @author CS 1302
+ * @version Fall 2025
+ */
 }
