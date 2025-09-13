76f68a57ad63dd6686e229f488501b3ce1fd0d8f
bellarusso <Irusso1@my.westga.edu>
2025-09-12 21:17:10 -0400

    Added specs for calculateTotal method

--- Lab4/src/edu/westga/cs1302/bill/model/BillCalculator.java
+++ Lab4/src/edu/westga/cs1302/bill/model/BillCalculator.java
@@ -1,10 +1,18 @@
 package edu.westga.cs1302.bill.model;
-
-public class BillCalculator {
 /** 
  * gives static method to calculate bill total
  * 
  * @author CS 1302
  * @version Fall 2025
  */
+public final class BillCalculator {
+	private BillCalculator() {
 }
+	/**
+	 * Calculates the total of items
+	 * 
+	 * @param items the array of BillItem objects
+	 * @return the subtotal
+	 */
+	
+	
